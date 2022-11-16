fc3a320ae163c7bb312f8a93da7aa11140ee50e5
Xtian <xtiancuenta1@gmail.com>
2022-11-16 17:55:19 -0300

    Ejercicio 1 hecho

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        
+        int[] vector = new int[5];
+
+        Scanner entrada = new Scanner(System.in);
+
+        for (int i=0; i<5; i++) {
+            System.out.println("Ingrese un número entero para la posición "+i);
+            vector[i]= entrada.nextInt();
+        }
+
+
+        for (int i=0; i<5; i++) {
+            System.out.println("["+i+"] = "+vector[i]);
+        }
     }
 }
